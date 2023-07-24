cbfcf1b31026fa0b7f0cbd009249ee7c527df72b
sickCoder <lunagio683@gmail.com>
2023-07-23 21:45:48 -0700

    fix

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+interface TodoItemProps{
+    text: string
+}
+const TodoItem: React.FC<TodoItemProps> = ({ text }) => {
+  return <li>{text}</li>;
+};
+
+export default TodoItem;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,40 @@
 import React, { useState } from 'react';
 import { InputGroup, Form, Button } from 'react-bootstrap';
-
-
+import TodoItem from './TodoItem'; // Import the TodoItem component
 const TodoList = () => {
+
+    
+    const [todo, addTodo] = useState<string>('');
+    const [list,addList] = useState<string[]>([]);
+
+    function handleSubmit(e:React.FormEvent<HTMLFormElement>){// 
+        e.preventDefault();
+        if(todo.trim() !== ''){
+          addList([...list,todo]);
+          addTodo('');
+        }
+    }
   return (
     <div>
-      <InputGroup className="mb-3 inputText">
-        <Form.Control
-          placeholder="New task..."
-          aria-label="New task..."
-          aria-describedby="basic-addon2"
-        />
-      </InputGroup>
-      <Button variant="primary">Add</Button>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-3 inputText">
+          <Form.Control
+
+            placeholder="New task..."
+            aria-label="New task..."
+            aria-describedby="basic-addon2"
+            onChange={(e) => addTodo(e.target.value)} 
+
+          />
+        </InputGroup>
+        <Button type="submit"variant="primary">Add</Button>
+      </Form>
+      <hr/>
+      <ul className='listItems'>
+      {list.map((task, index) => (
+          <TodoItem key={index} text={task} />
+        ))}
+      </ul>
     </div>
   );
 };
